ef7b091cc6468a8a5fb0a12e3c212b566dee2d0e
Konrad <konrad.wojtylo.9@gmail.com>
2023-09-24 13:32:14 +0200

    Added promises exercises

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -19,6 +19,8 @@ import { blockRouterMetaData as blockRouterMetaDataObjectOrientedProgramming } f
 import { blockRouterMetaData as blockRouterMetaDataObjectOrientedProgrammingExercises } from './ObjectOrientedProgrammingExercises/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsonXmlStorage } from './JsonXmlStorage/router-data';
 import { blockRouterMetaData as blockRouterMetaDataSavedInput } from './SavedInput/router-data';
+import { blockRouterMetaData as blockRouterMetaDataPromisesAndMe } from './PromisesAndMe/router-data';
+import { blockRouterMetaData as blockRouterDataUseEffectAndPromiseExercise } from './UseEffectAndPromiseExercise/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
@@ -40,5 +42,7 @@ export const blockRouterMetaData = [
   blockRouterMetaDataObjectOrientedProgrammingExercises,
   blockRouterMetaDataJsonXmlStorage,
   blockRouterMetaDataSavedInput,
+  blockRouterMetaDataPromisesAndMe,
+  blockRouterDataUseEffectAndPromiseExercise,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
