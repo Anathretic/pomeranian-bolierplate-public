392f5a20818da59cabcf0edb7b90affb9f18877a
Konrad <konrad.wojtylo.9@gmail.com>
2023-10-08 12:57:48 +0200

    Made a login and register form with firebase

--- src/App/Exercises/ReactExercises/AuthFirebase/index.jsx
+++ src/App/Exercises/ReactExercises/AuthFirebase/index.jsx
@@ -1,6 +1,32 @@
-import { firebaseConfig } from '../../../Firebase/firebaseConfig';
+import React from 'react';
+import { AuthUser } from './AuthUser';
+import { app } from '../../../Firebase/firebaseConfig';
+import { logout } from '../../../Firebase/firebaseClient';
+import { useAuthStatus } from './useAuthStatus';
+
+import './styles.css';
 
 export const AuthFirebase = () => {
-  console.log(firebaseConfig);
-  return <div>AuthFirebase</div>;
+  const { isLoggedIn, user } = useAuthStatus();
+
+  return (
+    <>
+      <h1>Autoryzacja z Firebase</h1>
+      {isLoggedIn === null ? null : (
+        <div>
+          <p>Zainicjowany firebase..</p>
+          {isLoggedIn === 'yes' ? (
+            <>
+              <div>Jesteś zalogowany</div>
+              <button type="button" onClick={logout}>
+                Wyloguj
+              </button>
+            </>
+          ) : (
+            <AuthUser />
+          )}
+        </div>
+      )}
+    </>
+  );
 };
