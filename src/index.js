ffac529e861cae420cd4fc008e624631d50cff10
Konrad <konrad.wojtylo.9@gmail.com>
2023-11-05 12:10:20 +0100

    Worked on mocks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { worker } from './Mocks/setupWorker';
 
 import './index.css';
 import { App } from './App';
+
+worker.start({ onUnhandledRequest: 'bypass' });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
